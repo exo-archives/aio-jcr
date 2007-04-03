251ff310d728a5e9b9f3368555435d9dbcb73deb
Sergey  Kabashnyuk <sergey.kabashnyuk@exoplatform.com>
2007-04-03 09:01:46 +0000

    Add constructor QPathEntry(InternalQName qName, int index)

--- component/core/src/main/java/org/exoplatform/services/jcr/datamodel/QPathEntry.java
+++ component/core/src/main/java/org/exoplatform/services/jcr/datamodel/QPathEntry.java
@@ -6,7 +6,11 @@ package org.exoplatform.services.jcr.datamodel;
 public class QPathEntry extends InternalQName {
 
     private final int index;
-
+    public QPathEntry(InternalQName qName, int index){
+      super(qName.getNamespace(), qName.getName());
+      this.index = index > 0 ? index : 1;
+      
+    } 
     public QPathEntry(String namespace, String name, int index) {
       super(namespace, name);
       this.index = index > 0 ? index : 1;
