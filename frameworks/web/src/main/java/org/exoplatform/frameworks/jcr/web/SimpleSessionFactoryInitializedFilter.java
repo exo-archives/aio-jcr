b667ccf43253f04aa52d1c4267cdbf874d6903a6
Vitaliy Obmanyuk <vitaliy.obmanyuk@exoplatform.com>
2007-06-13 07:34:31 +0000

    authenticationService.setCurrentIdentity(identity) section added

--- frameworks/web/src/main/java/org/exoplatform/frameworks/jcr/web/SimpleSessionFactoryInitializedFilter.java
+++ frameworks/web/src/main/java/org/exoplatform/frameworks/jcr/web/SimpleSessionFactoryInitializedFilter.java
@@ -7,9 +7,12 @@ package org.exoplatform.frameworks.jcr.web;
 
 import java.io.IOException;
 
+import javax.jcr.SimpleCredentials;
+import javax.jcr.Credentials;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.security.auth.Subject;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -24,122 +27,135 @@ import org.apache.commons.logging.Log;
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.PortalContainer;
-/*import org.exoplatform.container.SessionContainer;
-import org.exoplatform.container.SessionManager;
-import org.exoplatform.container.SessionManagerContainer;
-import org.exoplatform.container.client.http.HttpClientInfo;*/
 import org.exoplatform.frameworks.jcr.SingleRepositorySessionFactory;
 import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.config.RepositoryConfigurationException;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.organization.auth.AuthenticationService;
+import org.exoplatform.services.organization.auth.Identity;
 import org.exoplatform.services.security.SecurityService;
 
 /**
- * Created by The eXo Platform SARL        .
+ * Created by The eXo Platform SARL .
+ * 
  * @author <a href="mailto:gennady.azarenkov@exoplatform.com">Gennady Azarenkov</a>
- * @version $Id: SimpleSessionFactoryInitializedFilter.java 7163 2006-07-19 07:30:39Z peterit $
+ * @version $Id: SimpleSessionFactoryInitializedFilter.java 7163 2006-07-19
+ *          07:30:39Z peterit $
  */
 
 public class SimpleSessionFactoryInitializedFilter implements Filter {
 
-  private static Log log = ExoLogger.getLogger("jcr.SimpleSessionFactoryInitializedFilter");
-  
-//  public static final String DEFAULT_USER_NAME = "org.exoplatform.sample.defaultUserName";
-//  public static final String DEFAULT_USER_PASSWORD = "org.exoplatform.sample.defaultUserPassword";
-  
+  private static Log     log    = ExoLogger.getLogger("jcr.SimpleSessionFactoryInitializedFilter");
+
+  // public static final String DEFAULT_USER_NAME =
+  // "org.exoplatform.sample.defaultUserName";
+  // public static final String DEFAULT_USER_PASSWORD =
+  // "org.exoplatform.sample.defaultUserPassword";
+
   private ServletContext servletContext;
-  
-  private String userId = null;
-  
-  
+
+  private String         userId = null;
+
   public void init(FilterConfig config) throws ServletException {
     servletContext = config.getServletContext();
   }
 
-  public void doFilter(ServletRequest request, ServletResponse response,
-      FilterChain chain) throws IOException, ServletException {
-    
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpSession httpSession = httpRequest.getSession();
-    
-    ////////////////////// 
+
     String portalName = servletContext.getServletContextName();
     ExoContainer container = ExoContainerContext.getContainerByName(portalName);
-    if (container == null) 
+    if (container == null)
       container = ExoContainerContext.getTopContainer();
-    
-    ExoContainerContext.setCurrentContainer(container);
-    
-    SecurityService securityService = (SecurityService) container.getComponentInstanceOfType(SecurityService.class);
-    securityService.setCurrentUser(httpRequest.getRemoteUser());
-    
-    
-    /*SessionManagerContainer sessionManagerContainer = (SessionManagerContainer) container;
-    SessionManager sessionManager = sessionManagerContainer.getSessionManager();
-    SessionContainer sessionContainer = sessionManager.getSessionContainer(httpSession.getId());
-    if (sessionContainer == null) {
-      sessionContainer = sessionManagerContainer.createSessionContainer(httpSession
-          .getId(), "default");
-      sessionContainer.setClientInfo(new HttpClientInfo(httpRequest));
-    }
-    // attach to current thread
-    SessionContainer.setInstance(sessionContainer);*/
-    
-    /////////////////////
-    try {
 
-      if (httpRequest.getSession().getAttribute(
-          SingleRepositorySessionFactory.SESSION_FACTORY) == null
-          || userChanged(httpRequest, securityService )) {
-        
-//        SimpleCredentials cred;
+    ExoContainerContext.setCurrentContainer(container);
+
+    AuthenticationService authenticationService = (AuthenticationService) container
+        .getComponentInstanceOfType(AuthenticationService.class);
+    if (httpRequest.getRemoteUser() != null) {
+      if (authenticationService.getCurrentIdentity() == null) {
+        log.warn("Cannot find the identity for user " + httpRequest.getRemoteUser()
+            + ", trying to create the new one");
+        Identity identity = null;
+        try {
+        identity = authenticationService.getIdentityBySessionId(httpRequest.getRemoteUser());
+        }catch (Exception e) {
+          log.error("Can't find identity by sessionID ", e);
+        }
+        authenticationService.setCurrentIdentity(identity);
+      }
+    }
+    //
+    /*
+     * SessionManagerContainer sessionManagerContainer =
+     * (SessionManagerContainer) container; SessionManager sessionManager =
+     * sessionManagerContainer.getSessionManager(); SessionContainer
+     * sessionContainer =
+     * sessionManager.getSessionContainer(httpSession.getId()); if
+     * (sessionContainer == null) { sessionContainer =
+     * sessionManagerContainer.createSessionContainer(httpSession .getId(),
+     * "default"); sessionContainer.setClientInfo(new
+     * HttpClientInfo(httpRequest)); } // attach to current thread
+     * SessionContainer.setInstance(sessionContainer);
+     */
+
+    try {
+      if (httpRequest.getSession().getAttribute(SingleRepositorySessionFactory.SESSION_FACTORY) == null
+          || userChanged(httpRequest, authenticationService)) {
+
         ManageableRepository rep;
-//        cred = new SimpleCredentials(servletContext
-//            .getInitParameter(DEFAULT_USER_NAME), servletContext
-//            .getInitParameter(DEFAULT_USER_PASSWORD).toCharArray());
+        // cred = new SimpleCredentials(servletContext
+        // .getInitParameter(DEFAULT_USER_NAME), servletContext
+        // .getInitParameter(DEFAULT_USER_PASSWORD).toCharArray());
 
         // (1)try to get Repository from JNDI
-        String repositoryName = servletContext
-            .getInitParameter(WebConstants.REPOSITORY_JNDI_NAME);
-        
+        String repositoryName = servletContext.getInitParameter(WebConstants.REPOSITORY_JNDI_NAME);
+
         if (repositoryName != null) {
           try {
             Context ctx = new InitialContext();
             rep = (ManageableRepository) ctx.lookup(repositoryName);
-            log.info("Repository found in the JNDI context "+rep+" InitialContextFactory: "+System.getProperty(Context.INITIAL_CONTEXT_FACTORY));
+            log.info("Repository found in the JNDI context " + rep + " InitialContextFactory: "
+                + System.getProperty(Context.INITIAL_CONTEXT_FACTORY));
+            RepositoryService repositoryService = (RepositoryService) container
+            .getComponentInstanceOfType(RepositoryService .class);
+            repositoryService.setCurrentRepositoryName(repositoryName);
           } catch (NamingException e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+          } catch (RepositoryConfigurationException e) {
             e.printStackTrace();
             throw new ServletException(e);
           }
 
         } else {
           log.info("No Repository object found in the JNDI context. Try to get from container");
-
-
           RepositoryService service = (RepositoryService) container
               .getComponentInstanceOfType(RepositoryService.class);
           try {
-            rep = service.getRepository();
+            rep = service.getDefaultRepository();
           } catch (Exception e) {
             e.printStackTrace();
             throw new ServletException(e);
           }
         }
 
-
-//        httpRequest.getSession().setAttribute(
-//            SingleRepositorySessionFactory.SESSION_FACTORY,
-//            new SingleRepositorySessionFactory(rep, cred));
-        httpRequest.getSession().setAttribute(
-            SingleRepositorySessionFactory.SESSION_FACTORY,
+        // httpRequest.getSession().setAttribute(
+        // SingleRepositorySessionFactory.SESSION_FACTORY,
+        // new SingleRepositorySessionFactory(rep, cred));
+        httpRequest.getSession().setAttribute(SingleRepositorySessionFactory.SESSION_FACTORY,
             new SingleRepositorySessionFactory(rep));
-
+            //new SingleRepositorySessionFactory(rep, new Credentials("admin", "admin".toCharArray())));
       }
-      
+
       chain.doFilter(request, response);
-     
+
     } finally {
-      //SessionContainer.setInstance(null);
+      // SessionContainer.setInstance(null);
       PortalContainer.setInstance(null);
     }
 
@@ -147,26 +163,38 @@ public class SimpleSessionFactoryInitializedFilter implements Filter {
 
   public void destroy() {
   }
-  
-  //private boolean userChanged(HttpServletRequest httpRequest, SessionContainer sessionContainer) {
-  private boolean userChanged(HttpServletRequest httpRequest, SecurityService securityService) {
-    
+
+  // private boolean userChanged(HttpServletRequest httpRequest,
+  // SessionContainer sessionContainer) {
+  private boolean userChanged(HttpServletRequest httpRequest,
+      AuthenticationService authenticationService) {
+
     String newUser = httpRequest.getRemoteUser();
-    
+
     boolean res = true;
-    if(userId == null) {
-      if(newUser == null)
+    if (userId == null) {
+      if (newUser == null)
         res = false;
     } else {
       res = !userId.equals(newUser);
     }
-//    System.out.println("---------------- "+res+" "+userId+" "+newUser);
-    if(res) {
+    // System.out.println("---------------- "+res+" "+userId+" "+newUser);
+    if (res) {
       // refresh sessionContainer
-      securityService.setCurrentUser(newUser);
-      /*sessionContainer.setClientInfo(new HttpClientInfo(httpRequest));
-      SessionContainer.setInstance(sessionContainer);*/
-
+      if (newUser != null) {
+        Identity identity = null;
+        try {
+        identity = authenticationService.getIdentityBySessionId(newUser);
+        }catch (Exception e) {
+          log.error("Can't find identity by sessionID ", e);
+        }
+        authenticationService.setCurrentIdentity(identity);
+      }
+      // securityService.setCurrentUser(newUser);
+      /*
+       * sessionContainer.setClientInfo(new HttpClientInfo(httpRequest));
+       * SessionContainer.setInstance(sessionContainer);
+       */
       userId = newUser;
     }
     return res;
