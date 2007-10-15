11ee75682c744272b835ed4de2c8bdcb69e0736d
John Doe <noreply@exoplatform.com>
2007-10-15 08:20:37 +0000

    SVM: skipping changes 20730-20731 for svn://svn.forge.objectweb.org/svnroot/exoplatform/projects

--- component/core/src/main/java/org/exoplatform/services/jcr/impl/core/WorkspaceImpl.java
+++ component/core/src/main/java/org/exoplatform/services/jcr/impl/core/WorkspaceImpl.java
@@ -430,15 +430,6 @@ public class WorkspaceImpl implements ExtendedWorkspace {
       }
     }
 
-    // TODO [PN] 06.07.07 Commented as this is out of JSR-170
-//    // Check if versionable ancestor is not checked-in
-//    if (!destParentNode.isCheckedOut())
-//      throw new VersionException("Source parent node " + srcNode.getPath()
-//          + " or its nearest ancestor is checked-in");
-//    
-//    // Check locking
-//    if (!destParentNode.checkLocking())
-//      throw new LockException("Source parent node " + srcNode.getPath() + " is     locked ");
 
     ItemDataCloneVisitor initializer = new ItemDataCloneVisitor((NodeData) destParentNode.getData(),
         destNodePath.getName().getInternalName(),
