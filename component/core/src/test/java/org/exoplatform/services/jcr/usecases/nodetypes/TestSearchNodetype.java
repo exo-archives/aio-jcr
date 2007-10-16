a6fe54465a92747daa9d959605754ed9dd48fcf2
Volodymyk Krasnokiv <volodymyr.krasnikov@exoplatform.com.ua>
2007-10-16 10:34:02 +0000

    add test for search rma:record nodetype

--- component/core/src/test/java/org/exoplatform/services/jcr/usecases/nodetypes/TestSearchNodetype.java
+++ component/core/src/test/java/org/exoplatform/services/jcr/usecases/nodetypes/TestSearchNodetype.java
@@ -21,6 +21,7 @@ import org.exoplatform.services.jcr.usecases.BaseUsecasesTest;
  */
 
 public class TestSearchNodetype extends BaseUsecasesTest {
+	
 	public void testCreateNodetype() throws Exception {
 
 		log.info("!!Adding node with test nodetype");
@@ -38,8 +39,8 @@ public class TestSearchNodetype extends BaseUsecasesTest {
 					.setProperty("rma:originatingOrganization", "testProperty2");
 		}
 
-		Node testNode1 = someNode.addNode("Test1");
-		Node testNode2 = someNode.addNode("Test2");
+		someNode.addNode("Test1");
+		someNode.addNode("Test2");
 		
 		session.save();
 		
@@ -54,8 +55,47 @@ public class TestSearchNodetype extends BaseUsecasesTest {
 		assertTrue( iter.getSize() == 1 );	// check target node for existanse
 		assertNotNull( target = iter.nextNode() );
 		
-		testNode1 = target.getNode("Test1");
-		testNode2 = target.getNode("Test2");
+		target.getNode("Test1");
+		target.getNode("Test2");
+				
+				
+	}
+	
+	public void testCreateNodetypeWithLogout() throws Exception{
+
+		log.info("!!Adding node with test nodetype - USE LOGOUT/LOGIN");
+
+		Node rootNode = session.getRootNode();
+		Node queryNode = rootNode.addNode("queryNode", "nt:unstructured");
+		Node someNode = queryNode.addNode("smth", "nt:unstructured");
+
+		if (!someNode.canAddMixin("rma:record"))
+			throw new RepositoryException("Cannot add mixin node");
+		else {
+			someNode.addMixin("rma:record");
+			someNode.setProperty("rma:recordIdentifier", "testIdentificator");
+			someNode
+					.setProperty("rma:originatingOrganization", "testProperty2");
+		}
+
+		someNode.addNode("Test1");
+		someNode.addNode("Test2");
+		
+		session.save();
+		
+		String sqlQuery = "SELECT * FROM rma:record WHERE jcr:path LIKE '/queryNode/smth' ";
+		QueryManager manager = session.getWorkspace().getQueryManager();
+		Query query = manager.createQuery(sqlQuery, Query.SQL);
+
+		QueryResult queryResult = query.execute();
+		NodeIterator iter = queryResult.getNodes();
+		
+		Node target = null;
+		assertTrue( iter.getSize() == 1 );	// check target node for existanse
+		assertNotNull( target = iter.nextNode() );
+		
+		Node testNode1 = target.getNode("Test1");
+		Node testNode2 = target.getNode("Test2");
 		
 		session.logout();
 		
@@ -71,7 +111,58 @@ public class TestSearchNodetype extends BaseUsecasesTest {
 		
 		testNode1 = target.getNode("Test1");
 		testNode2 = target.getNode("Test2");
+	
+	}
+	
+	public void testCreateNodetypeWithPreQueryManader() throws Exception{
+		log.info("!!Adding node with test nodetype - USE LOGOUT/LOGIN");
 		
-				
+		String sqlQuery = "SELECT * FROM rma:record WHERE jcr:path LIKE '/queryNode/smth' ";
+		QueryManager manager = session.getWorkspace().getQueryManager();
+		Query query = manager.createQuery(sqlQuery, Query.SQL);
+
+		Node rootNode = session.getRootNode();
+		Node queryNode = rootNode.addNode("queryNode", "nt:unstructured");
+		Node someNode = queryNode.addNode("smth", "nt:unstructured");
+
+		if (!someNode.canAddMixin("rma:record"))
+			throw new RepositoryException("Cannot add mixin node");
+		else {
+			someNode.addMixin("rma:record");
+			someNode.setProperty("rma:recordIdentifier", "testIdentificator");
+			someNode
+					.setProperty("rma:originatingOrganization", "testProperty2");
+		}
+
+		someNode.addNode("Test1");
+		someNode.addNode("Test2");
+		
+		session.save();
+		
+		QueryResult queryResult = query.execute();
+		NodeIterator iter = queryResult.getNodes();
+		
+		Node target = null;
+		assertTrue( iter.getSize() == 1 );	// check target node for existanse
+		assertNotNull( target = iter.nextNode() );
+		
+		Node testNode1 = target.getNode("Test1");
+		Node testNode2 = target.getNode("Test2");
+		
+		session.logout();
+		
+		// new login
+		session = (SessionImpl) repository.login(credentials, "ws");
+		
+		Query query2 = manager.createQuery(sqlQuery, Query.SQL);
+		QueryResult queryResult2 = query2.execute();
+		NodeIterator iter2 = queryResult2.getNodes();
+		
+		assertTrue( iter2.getSize() == 1 ); // check target node for existanse
+		assertNotNull( target = iter2.nextNode() );
+		
+		testNode1 = target.getNode("Test1");
+		testNode2 = target.getNode("Test2");
+
 	}
 }
