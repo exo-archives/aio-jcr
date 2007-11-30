2e869083274404b8df83ae7f60f6e51e99ec00b9
John Doe <noreply@exoplatform.com>
2007-11-30 15:52:29 +0000

    SVM: skipping changes 22269-22270 for svn://svn.forge.objectweb.org/svnroot/exoplatform/projects

--- component/core/src/main/java/org/exoplatform/services/jcr/impl/core/version/FrozenNodeInitializer.java
+++ component/core/src/main/java/org/exoplatform/services/jcr/impl/core/version/FrozenNodeInitializer.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see<http://www.gnu.org/licenses/>.
  */
-
 package org.exoplatform.services.jcr.impl.core.version;
 
 import java.util.ArrayList;
--- component/core/src/main/java/org/exoplatform/services/jcr/impl/xml/importing/BaseXmlImporter.java
+++ component/core/src/main/java/org/exoplatform/services/jcr/impl/xml/importing/BaseXmlImporter.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see<http://www.gnu.org/licenses/>.
  */
-
 package org.exoplatform.services.jcr.impl.xml.importing;
 
 import java.util.ArrayList;
