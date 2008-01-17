4ea7899b54208ec33c47a1cd8cb8e5f0ddb8f8b2
Peter Nedonosko <pnedonosko@exoplatform.com>
2008-01-17 10:16:19 +0000

    TestNodeRestoreMixin nodetypes moved to in test/resources/conf/test/nodetypes-usecase.xml

--- component/core/src/test/java/org/exoplatform/services/jcr/usecases/nodetypes/TestNodeRemoveMixin.java
+++ component/core/src/test/java/org/exoplatform/services/jcr/usecases/nodetypes/TestNodeRemoveMixin.java
@@ -16,13 +16,8 @@
  */
 package org.exoplatform.services.jcr.usecases.nodetypes;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-
 import javax.jcr.Node;
 
-import org.exoplatform.services.jcr.impl.core.nodetype.NodeTypeManagerImpl;
 import org.exoplatform.services.jcr.usecases.BaseUsecasesTest;
 
 /**
@@ -35,15 +30,8 @@ public class TestNodeRemoveMixin extends BaseUsecasesTest {
 
   public void testNodeRemoveMixin() throws Exception{
     
-    //Register Nodetypes
-    byte[] xmlData = readXmlContent("/org/exoplatform/services/jcr/usecases/nodetypes/nodetypes-usecase-test.xml");
+    //Register Nodetypes - performed in configuration
 
-    ByteArrayInputStream xmlInput = new ByteArrayInputStream(xmlData);
-
-    NodeTypeManagerImpl ntManager = (NodeTypeManagerImpl) session.getWorkspace()
-        .getNodeTypeManager();
-    ntManager.registerNodeTypes(xmlInput, 0);
-    
     String nodeType = "exo:archiveable";
     
     //Create Node
@@ -74,33 +62,5 @@ public class TestNodeRemoveMixin extends BaseUsecasesTest {
     
     //Error should not be here!
     assertNotNull(testNode.getProperties());       
-    
-    
-  }
-  
-  private byte[] readXmlContent(String fileName) {
-
-    try {
-      InputStream is = TestNodeTypeRegisterReferenced.class.getResourceAsStream(fileName);
-      ByteArrayOutputStream output = new ByteArrayOutputStream();
-
-      int r = is.available();
-      byte[] bs = new byte[r];
-      while (r > 0) {
-        r = is.read(bs);
-        if (r > 0) {
-          output.write(bs, 0, r);
-        }
-        r = is.available();
-      }
-      is.close();
-      return output.toByteArray();
-    } catch (Exception e) {
-      log.error("Error read file '" + fileName + "' with NodeTypes. Error:" + e);
-      return null;
-    }
-  }
-  
-  
-  
+  }  
 }
