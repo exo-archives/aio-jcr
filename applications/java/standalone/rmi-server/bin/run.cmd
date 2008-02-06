dd2afa9b2434287835b68c41d8018dce2b0dca87
Vitaliy Obmanuyk <vitaliy.obmanyuk@exoplatform.com.ua>
2008-02-06 16:22:50 +0000

    trunk version

--- applications/java/standalone/rmi-server/bin/run.cmd
+++ applications/java/standalone/rmi-server/bin/run.cmd
@@ -1,4 +1,4 @@
 set dbin=%~dp0
 set codebase=%dbin%\..\codebase\
 set confpath=%dbin%\..\config\
-java -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog  -Djava.security.policy=%confpath%java.policy -Djava.security.auth.login.config=%confpath%jaas.conf -Djava.rmi.server.codebase=file:/%codebase% -jar ../lib/exo.jcr.applications.rmi-server-1.6.jar bind %confpath%rmi-server-configuration.xml
+java -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog  -Djava.security.policy=%confpath%java.policy -Djava.security.auth.login.config=%confpath%jaas.conf -Djava.rmi.server.codebase=file:/%codebase% -jar ../lib/exo.jcr.applications.rmi-server-trunk.jar bind %confpath%rmi-server-configuration.xml
