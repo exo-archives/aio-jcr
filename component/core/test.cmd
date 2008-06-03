30cf3252ead77f9f6424f0f07be22c8784cf8e87
Peter Nedonosko <pnedonosko@exoplatform.com>
2008-06-03 10:45:33 +0000

    JCR core dependencies cleanup (dbcp 1.2.2 instead of 1.2.1)

--- component/core/test.cmd
+++ component/core/test.cmd
@@ -1,7 +1,5 @@
-@ECHO ========================== RUN ===========================
+@REM SET MAVEN_OPTS=-Xmx256m -Duser.language=en -Duser.region=us %MAVEN_OPTS% -Djava.io.tmpdir="D:\Tmp" -Dexo.test.skip=false -Dexo.tck.skip=true -Dexo.test.forkMode=once 
 
-@REM @set JAVA_HOME=%JDK5_HOME%
-
-@SET MAVEN_OPTS=-Duser.language=en -Duser.region=us %MAVEN_OPTS% -Djava.io.tmpdir="C:\Tmp"
+@SET MAVEN_OPTS=-Xmx256m -Duser.language=en -Duser.region=us %MAVEN_OPTS% -Dexo.test.skip=false -Dexo.tck.skip=true -Dexo.test.forkMode=once 
 
 @start mvn clean test
