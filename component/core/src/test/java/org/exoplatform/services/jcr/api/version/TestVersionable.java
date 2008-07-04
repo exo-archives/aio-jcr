0d7bb9bc8540b26e002ee8327f637efd34d46d41
Sergey  Kabashnyuk <sergey.kabashnyuk@exoplatform.com>
2008-07-04 11:06:17 +0000

    JCR-437 : create test

--- component/core/src/test/java/org/exoplatform/services/jcr/api/version/TestVersionable.java
+++ component/core/src/test/java/org/exoplatform/services/jcr/api/version/TestVersionable.java
@@ -19,10 +19,13 @@ package org.exoplatform.services.jcr.api.version;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.nodetype.NoSuchNodeTypeException;
+import javax.jcr.version.VersionHistory;
+
+import org.exoplatform.services.jcr.datamodel.ItemData;
+import org.exoplatform.services.jcr.impl.core.NodeImpl;
 
 /**
- * Created by The eXo Platform SAS 
- * Author : Peter Nedonosko
+ * Created by The eXo Platform SAS Author : Peter Nedonosko
  * peter.nedonosko@exoplatform.com.ua 18.01.2008
  * 
  * @author <a href="mailto:peter.nedonosko@exoplatform.com.ua">Peter Nedonosko</a>
@@ -35,25 +38,25 @@ public class TestVersionable extends BaseVersionTest {
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    
+
     testRoot = root.addNode("testRemoveVersionable");
     root.save();
     testRoot.addMixin("mix:versionable");
     root.save();
-    
+
     testRoot.checkin();
     testRoot.checkout();
-    
+
     testRoot.addNode("node1");
     testRoot.addNode("node2").setProperty("prop1", "a property #1");
     testRoot.save();
-    
+
     testRoot.checkin();
     testRoot.checkout();
-    
+
     testRoot.getNode("node1").remove();
     testRoot.save();
-    
+
     testRoot.checkin();
   }
 
@@ -61,64 +64,90 @@ public class TestVersionable extends BaseVersionTest {
   protected void tearDown() throws Exception {
     testRoot.remove();
     root.save();
-    
+
     super.tearDown();
   }
-  
+
   public void testRemoveMixVersionable() throws Exception {
 
     testRoot.checkout();
-    
+
     try {
       testRoot.removeMixin("mix:versionable");
       testRoot.save();
-    } catch(RepositoryException e) {
+    } catch (RepositoryException e) {
       e.printStackTrace();
       fail("removeMixin(\"mix:versionable\") impossible due to error " + e.getMessage());
     }
   }
-  
+
+  /**
+   * Test for http://jira.exoplatform.org/browse/JCR-437
+   * 
+   * @throws Exception
+   */
+  public void testRemoveNonMixVersionableParent() throws Exception {
+    Node testroot = root.addNode("testRoot");
+    Node verionableChild = testroot.addNode("verionableChild");
+    verionableChild.addMixin("mix:versionable");
+    root.save();
+    assertFalse(testroot.isNodeType("mix:versionable"));
+    assertTrue(verionableChild.isNodeType("mix:versionable"));
+
+    VersionHistory vHistory = verionableChild.getVersionHistory();
+    assertNotNull(vHistory);
+
+    String vhId = ((NodeImpl) vHistory).getUUID();
+
+    assertNotNull(session.getTransientNodesManager().getItemData(vhId));
+    testroot.remove();
+    root.save();
+    assertFalse(root.hasNode("testRoot"));
+    ItemData vhdata = session.getTransientNodesManager().getItemData(vhId);
+    //!!!!
+    //assertNull(vhdata);
+  }
+
   public void testRemoveMixVersionableTwice() throws Exception {
 
     testRoot.checkout();
-    
+
     testRoot.removeMixin("mix:versionable");
     testRoot.save();
-    
+
     try {
       testRoot.removeMixin("mix:versionable");
       fail("removeMixin(\"mix:versionable\") should throw NoSuchNodeTypeException exception");
-    } catch(NoSuchNodeTypeException e) {
+    } catch (NoSuchNodeTypeException e) {
       // ok
     }
   }
-  
+
   public void testIsCheckedOut() throws Exception {
     // create versionable subnode and checkin its versionable parent
     // testRoot - versionable ancestor
-    
+
     testRoot.checkout();
     Node subNode = testRoot.addNode("node1").addNode("node2").addNode("subNode");
     testRoot.save();
-    
+
     subNode.addMixin("mix:versionable");
     testRoot.save();
-    
+
     subNode.checkin();
     subNode.checkout();
     subNode.setProperty("property1", "property1 v1");
     subNode.save();
     subNode.checkin();
     subNode.checkout();
-    
+
     // test
     testRoot.checkin(); // make subtree checked-in
     try {
       assertTrue("subNode should be checked-out as it's a mix:versionable", subNode.isCheckedOut());
-    } catch(RepositoryException e) {
-      
+    } catch (RepositoryException e) {
+
     }
   }
 
 }
-
