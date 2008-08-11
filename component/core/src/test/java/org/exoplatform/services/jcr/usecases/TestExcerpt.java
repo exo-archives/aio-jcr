b233f75512f3a86b3c5865de7c06b6274bb5b785
Sergey  Kabashnyuk <sergey.kabashnyuk@exoplatform.com>
2008-08-11 09:49:50 +0000

    TestQuery removed as duplicated. See  http://jira.exoplatform.org/browse/JCR-486 Add load nodetypes to testExcerpt

--- component/core/src/test/java/org/exoplatform/services/jcr/usecases/TestExcerpt.java
+++ component/core/src/test/java/org/exoplatform/services/jcr/usecases/TestExcerpt.java
@@ -16,7 +16,10 @@
  */
 package org.exoplatform.services.jcr.usecases;
 
+import java.io.InputStream;
+
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.jcr.Value;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
@@ -24,22 +27,44 @@ import javax.jcr.query.QueryResult;
 import javax.jcr.query.Row;
 import javax.jcr.query.RowIterator;
 
+import org.exoplatform.services.jcr.impl.core.nodetype.NodeTypeManagerImpl;
+
 /**
- * Created by The eXo Platform SAS Author : Ly Dinh Quang
- * quang.ly@exoplatform.com xxx5669@yahoo.com Aug 8, 2008
+ * Created by The eXo Platform SAS Author : Ly Dinh Quang quang.ly@exoplatform.com xxx5669@yahoo.com
+ * Aug 8, 2008
  */
 public class TestExcerpt extends BaseUsecasesTest {
-  private String s1 = "Additionally there is a parameter that controls the format of the excerpt created. "
-                        + "In JCR 1.9 the default is set to"
-                        + "org.exoplatform.services.jcr.impl.core.query.lucene.DefaultHTMLExcerpt. "
-                        + "the configuration parameter for this setting is:"
-                        + "This is the test for Excerpt query";
+  private String         s1            = "Additionally there is a parameter that controls the format of the excerpt created. "
+                                           + "In JCR 1.9 the default is set to"
+                                           + "org.exoplatform.services.jcr.impl.core.query.lucene.DefaultHTMLExcerpt. "
+                                           + "the configuration parameter for this setting is:"
+                                           + "This is the test for Excerpt query";
 
-  private String s2 = "It is a test for Excerpt query."
-                        + "Searching with synonyms is integrated in the jcr:contains() function and uses the same syntax "
-                        + "like synonym searches with Google. If a search term is prefixed with ~ also synonyms of the search term are considered. Example:";
+  private String         s2            = "It is a test for Excerpt query."
+                                           + "Searching with synonyms is integrated in the jcr:contains() function and uses the same syntax "
+                                           + "like synonym searches with Google. If a search term is prefixed with ~ also synonyms of the search term are considered. Example:";
 
-  private String s3 = "JCR supports such features as Lucene Fuzzy Searches";
+  private String         s3            = "JCR supports such features as Lucene Fuzzy Searches";
+
+  /**
+   * Initialization flag.
+   */
+  private static boolean isInitialized = false;
+
+  @Override
+  public void initRepository() throws RepositoryException {
+    super.initRepository();
+    if (!isInitialized) {
+      NodeTypeManagerImpl ntManager = session.getWorkspace().getNodeTypeManager();
+      InputStream is = TestExcerpt.class.getResourceAsStream("/nodetypes/ext-registry-nodetypes.xml");
+      ntManager.registerNodeTypes(is, 0);
+      ntManager.registerNodeTypes(TestExcerpt.class.getResourceAsStream("/org/exoplatform/services/jcr/api/nodetypes/ecm/nodetypes-config.xml"),
+                                  0);
+      ntManager.registerNodeTypes(TestExcerpt.class.getResourceAsStream("/org/exoplatform/services/jcr/api/nodetypes/ecm/nodetypes-config-extended.xml"),
+                                  0);
+      isInitialized = true;
+    }
+  }
 
   public void testExcerpt() throws Exception {
     System.out.println("\n\n----------Test Excerpt");
@@ -60,8 +85,8 @@ public class TestExcerpt extends BaseUsecasesTest {
 
     System.out.println("\n\n-----Test with SQL");
     QueryManager queryManager = session.getWorkspace().getQueryManager();
-    Query q1 = queryManager.createQuery(
-        "select excerpt(.) from exo:article where contains(., 'test')", Query.SQL);
+    Query q1 = queryManager.createQuery("select excerpt(.) from exo:article where contains(., 'test')",
+                                        Query.SQL);
     QueryResult result1 = q1.execute();
     for (RowIterator it = result1.getRows(); it.hasNext();) {
       Row r = it.nextRow();
@@ -71,7 +96,7 @@ public class TestExcerpt extends BaseUsecasesTest {
 
     System.out.println("\n\n-----Test with XPATH");
     Query q2 = queryManager.createQuery("//*[jcr:contains(., 'test')]/(@exo:text|rep:excerpt(.))",
-        Query.XPATH);
+                                        Query.XPATH);
     QueryResult result2 = q2.execute();
     for (RowIterator it = result2.getRows(); it.hasNext();) {
       Row r = it.nextRow();
