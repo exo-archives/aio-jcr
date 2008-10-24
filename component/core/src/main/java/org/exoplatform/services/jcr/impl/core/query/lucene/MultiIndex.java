85ff302fb1c369e62d1ff68b9fe799431b18a989
Sergey  Kabashnyuk <sergey.kabashnyuk@exoplatform.com>
2008-10-24 13:16:36 +0000

    add log.isDebugEnabled()

--- component/core/src/main/java/org/exoplatform/services/jcr/impl/core/query/lucene/MultiIndex.java
+++ component/core/src/main/java/org/exoplatform/services/jcr/impl/core/query/lucene/MultiIndex.java
@@ -958,7 +958,8 @@ public class MultiIndex {
       resetVolatileIndex();
 
       time = System.currentTimeMillis() - time;
-      log.debug("Committed in-memory index in " + time + "ms.");
+      if (log.isDebugEnabled())
+        log.debug("Committed in-memory index in " + time + "ms.");
     }
   }
 
