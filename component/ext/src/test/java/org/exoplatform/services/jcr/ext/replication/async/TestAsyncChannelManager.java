8e1586e8bb2f52aec40e582d6f7cd1c6e6765e31
Sergey Karpenko <sergey.karpenko@exoplatform.com>
2008-12-17 15:20:50 +0000

    JCR-674: test updated

---
+++ component/ext/src/test/java/org/exoplatform/services/jcr/ext/replication/async/TestAsyncChannelManager.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.services.jcr.ext.replication.async;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.exoplatform.services.jcr.ext.BaseStandaloneTest;
+import org.exoplatform.services.jcr.ext.replication.Packet;
+import org.exoplatform.services.jcr.ext.replication.PendingChangesLog;
+
+/**
+ * Created by The eXo Platform SAS Author : Karpenko Sergiy
+ * karpenko.sergiy@gmail.com.
+ */
+public class TestAsyncChannelManager extends BaseStandaloneTest {
+
+  protected abstract class TestPacketListener implements AsyncPacketListener {
+    abstract boolean isTested();
+    abstract List<AsyncPacket> getResievedPacketList();
+    
+  }
+
+  /**
+   * JChannel configuration.
+   */
+  private static final String CH_CONFIG = "TCP(oob_thread_pool.queue_max_size=100;thread_naming_pattern=cl;use_concurrent_stack=true;oob_thread_pool.rejection_policy=Run;discard_incompatible_packets=true;thread_pool.max_threads=40;oob_thread_pool.enabled=false;oob_thread_pool.max_threads=20;loopback=false;oob_thread_pool.keep_alive_time=5000;thread_pool.queue_enabled=false;oob_thread_pool.queue_enabled=false;max_bundle_size=64000;thread_pool.queue_max_size=100;thread_pool.enabled=false;enable_diagnostics=true;max_bundle_timeout=30;oob_thread_pool.min_threads=8;use_incoming_packet_handler=true;thread_pool.rejection_policy=Run;bind_addr=127.0.0.1;thread_pool.min_threads=8;thread_pool.keep_alive_time=5000;enable_bundling=true):MPING(timeout=2000;num_initial_members=8;mcast_port=34526;mcast_addr=224.0.0.1):FD(timeout=2000;max_tries=5;shun=true):FD_SOCK:VERIFY_SUSPECT(timeout=1500):pbcast.NAKACK(max_xmit_size=60000;print_stability_history_on_failed_xmit=true;use_mcast_xmit=false;gc_lag=0;discard_delivered_msgs=true;retransmit_timeout=300,600,1200,2400,4800):pbcast.STABLE(stability_delay=1000;desired_avg_gossip=50000;max_bytes=8000000):pbcast.GMS(print_local_addr=true;join_timeout=3000;view_bundling=true;join_retry_timeout=2000;shun=true;merge_leader=true;reject_join_from_existing_member=true)";
+
+  private static final String CH_NAME   = "TestChannel";
+
+  private AsyncChannelManager channel;
+
+  public void setUp() throws Exception {
+    // init channel
+    channel = new AsyncChannelManager(CH_CONFIG, CH_NAME);
+    channel.init();
+    channel.connect();
+  }
+  
+  public void tearDown() throws Exception {
+    channel.closeChannel();
+    channel = null;
+  }
+  
+  /**
+   * Test sendBigFile method.
+   * 
+   * @throws Exception internal exception
+   */
+  public void testSendBigFile() throws Exception {
+    String packetId = "Id";
+    String packetOwnName = "Owner";
+    
+    TestPacketListener listener = new TestPacketListener() {
+
+      private boolean isTested = false;
+      private List<AsyncPacket> list = new ArrayList<AsyncPacket>();
+      
+      public void receive(AsyncPacket packet) throws Exception {
+        assertNotNull(packet);
+        list.add(packet);
+        if(packet.getType() == AsyncPacketTypes.BIG_PACKET_LAST){
+          isTested = true;
+        }
+      }
+
+      @Override
+      boolean isTested() {
+        return isTested;
+      }
+
+      @Override
+      List<AsyncPacket> getResievedPacketList() {
+        return list;
+      }
+    };
+    
+    channel.addPacketListener(listener);
+
+    // send big file;
+    AsyncChannelManager tchannel = new AsyncChannelManager(CH_CONFIG, CH_NAME);
+    tchannel.init();
+    tchannel.connect();
+    
+    byte[] bigData = createBLOBTempData(256);
+    tchannel.sendBigPacket(bigData, new AsyncPacket(0, packetId, packetOwnName));
+    Thread.sleep(1000);
+    
+    assertEquals(true, listener.isTested());
+
+    List<AsyncPacket> list = listener.getResievedPacketList();
+    assertEquals(16,list.size());
+    assertEquals(AsyncPacketTypes.BIG_PACKET_FIRST, list.get(0).getType());
+    for(int i =1 ; i<list.size()-1; i++){
+      assertEquals(AsyncPacketTypes.BIG_PACKET_MIDDLE, list.get(i).getType());
+    }
+    assertEquals(AsyncPacketTypes.BIG_PACKET_LAST, list.get(list.size()-1).getType());
+    
+    // check Id and own name
+    for(int i =0 ; i<list.size(); i++){
+      assertEquals(packetId, list.get(i).getIdentifier());
+      assertEquals(packetOwnName, list.get(i).getOwnName());
+    }
+    
+    // check data
+    assertEquals(true, java.util.Arrays.equals(bigData, concatBigPacketBuffer(list)));
+    
+    //close channel
+    tchannel.closeChannel();
+  }
+ 
+  protected byte[] createBLOBTempData( int sizeInKb) throws IOException {
+    byte[] data = new byte[1024*sizeInKb]; // 1Kb
+    Random random = new Random();
+    random.nextBytes(data);
+    return data;
+  }
+  
+  private byte[] concatBigPacketBuffer( List<AsyncPacket> list){
+    //count result data size
+    int size=0;
+    for(int i =0 ; i<list.size(); i++){
+      size+=list.get(i).getBuffer().length;
+    }
+    
+    byte[] buf = new byte[size];
+    int pos =0;
+    for(int i = 0 ; i<list.size(); i++){
+      int length = list.get(i).getBuffer().length;
+      System.arraycopy(list.get(i).getBuffer(), 0, buf, pos , length);
+      pos+=length;
+    }
+    return buf;
+  }
+}
