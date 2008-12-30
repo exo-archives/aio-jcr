2865ca5d39cb4e165f36e5dbaf57c6ddd3b12655
Anatoliy Bazko <abazko@exoplatform.com>
2008-12-30 14:12:02 +0000

    JCR-663: MergerDataManager tests

---
+++ component/ext/src/test/java/org/exoplatform/services/jcr/ext/replication/async/TestMergerDataManager.java
@@ -0,0 +1,194 @@
+/**
+ * 
+ */
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.services.jcr.ext.replication.async;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jcr.InvalidItemStateException;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+
+import org.exoplatform.services.jcr.core.WorkspaceContainerFacade;
+import org.exoplatform.services.jcr.dataflow.DataManager;
+import org.exoplatform.services.jcr.dataflow.ItemState;
+import org.exoplatform.services.jcr.dataflow.PlainChangesLog;
+import org.exoplatform.services.jcr.dataflow.PlainChangesLogImpl;
+import org.exoplatform.services.jcr.dataflow.TransactionChangesLog;
+import org.exoplatform.services.jcr.ext.BaseStandaloneTest;
+import org.exoplatform.services.jcr.ext.replication.async.merge.CompositeChangesStorage;
+import org.exoplatform.services.jcr.ext.replication.async.storage.ChangesStorage;
+import org.exoplatform.services.jcr.ext.replication.async.transport.Member;
+import org.exoplatform.services.jcr.impl.core.SessionDataManagerTestWrapper;
+import org.exoplatform.services.jcr.impl.core.SessionImpl;
+import org.exoplatform.services.jcr.impl.core.WorkspaceImpl;
+import org.exoplatform.services.jcr.impl.dataflow.persistent.CacheableWorkspaceDataManager;
+
+/**
+ * Created by The eXo Platform SAS.
+ * 
+ * @author <a href="mailto:anatoliy.bazko@exoplatform.com.ua">Anatoliy Bazko</a>
+ * @version $Id: TestMergerDataManager.java 111 2008-11-11 11:11:11Z $
+ */
+public class TestMergerDataManager extends BaseStandaloneTest {
+
+  private final int                         LOCAL_PRIORITY = 10;
+
+  protected SessionImpl                     session3;
+
+  protected WorkspaceImpl                   workspace3;
+
+  protected Node                            root3;
+
+  protected SessionImpl                     session4;
+
+  protected WorkspaceImpl                   workspace4;
+
+  protected Node                            root4;
+
+  protected MergeDataManager                merger;
+
+  protected List<ChangesStorage<ItemState>> membersChanges;
+
+  /**
+   * {@inheritDoc}
+   */
+  public void setUp() throws Exception {
+    super.setUp();
+
+    session3 = (SessionImpl) repository.login(credentials, "ws3");
+    workspace3 = session3.getWorkspace();
+    root3 = session3.getRootNode();
+
+    session4 = (SessionImpl) repository.login(credentials, "ws4");
+    workspace4 = session4.getWorkspace();
+    root4 = session4.getRootNode();
+
+    merger = new MergeDataManager(new RemoteExporterImpl(null, null),
+                                  null,
+                                  null,
+                                  LOCAL_PRIORITY,
+                                  "target/storage");
+    membersChanges = new ArrayList<ChangesStorage<ItemState>>();
+  }
+
+  public void testAddMergeLocalPriority() throws Exception {
+    root3.addNode("item1");
+
+    SessionDataManagerTestWrapper dataManager = new SessionDataManagerTestWrapper(session3.getTransientNodesManager());
+    addChangesToChangesStorage(dataManager.getChangesLog(), LOCAL_PRIORITY - 1);
+    addChangesToChangesStorage(new PlainChangesLogImpl(), LOCAL_PRIORITY);
+
+    session3.save();
+
+    saveResultedChanges(merger.merge(membersChanges.iterator()).getChanges(), "ws4");
+    assertTrue(CompareWorkspaces());
+  }
+
+  /**
+   * CompareWorkspaces.
+   */
+  protected boolean CompareWorkspaces() {
+    // TODO
+    return true;
+  }
+
+  /**
+   * Add changes to changes storage.
+   * 
+   * @param log
+   * @param priority
+   */
+  protected void addChangesToChangesStorage(PlainChangesLog log, int priority) {
+    Member member = new Member(null, priority);
+    TransactionChangesLog cLog = new TransactionChangesLog(log);
+    CompositeChangesStorage<ItemState> changes = new CompositeChangesStorage<ItemState>(cLog,
+                                                                                        member);
+    membersChanges.add(changes);
+  }
+
+  /**
+   * Save resulted changes into workspace
+   * 
+   * @param res
+   * @throws RepositoryException
+   * @throws UnsupportedOperationException
+   * @throws InvalidItemStateException
+   */
+  protected void saveResultedChanges(Iterator<ItemState> res, String workspaceName) throws Exception {
+    WorkspaceContainerFacade wsc = repository.getWorkspaceContainer(workspaceName);
+    DataManager dm = (DataManager) wsc.getComponent(CacheableWorkspaceDataManager.class);
+
+    PlainChangesLog resLog = new PlainChangesLogImpl();
+
+    while (res.hasNext()) {
+      resLog.add(res.next());
+    }
+
+    dm.save(resLog);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void tearDown() throws Exception {
+
+    // clear ws3
+    if (session3 != null) {
+      try {
+        session3.refresh(false);
+        Node rootNode = session3.getRootNode();
+        if (rootNode.hasNodes()) {
+          for (NodeIterator children = rootNode.getNodes(); children.hasNext();) {
+            children.nextNode().remove();
+          }
+          session3.save();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        session4.logout();
+      }
+    }
+
+    // clear ws4
+    if (session4 != null) {
+      try {
+        session4.refresh(false);
+        Node rootNode = session4.getRootNode();
+        if (rootNode.hasNodes()) {
+          for (NodeIterator children = rootNode.getNodes(); children.hasNext();) {
+            children.nextNode().remove();
+          }
+          session4.save();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        session4.logout();
+      }
+    }
+
+    super.tearDown();
+  }
+
+}
