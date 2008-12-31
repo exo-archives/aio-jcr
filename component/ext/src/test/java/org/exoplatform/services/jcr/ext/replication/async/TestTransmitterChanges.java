9ff2fc16d5027695609f9fe014398cd5c189a2ad
Alexander  Reshetnyak <alex.reshetnyak@exoplatform.com>
2008-12-31 10:01:02 +0000

    JCR-671 :  TestTransmitterChanges

--- component/ext/src/test/java/org/exoplatform/services/jcr/ext/replication/async/TestTransmitterChanges.java
+++ component/ext/src/test/java/org/exoplatform/services/jcr/ext/replication/async/TestTransmitterChanges.java
@@ -16,18 +16,22 @@
  */
 package org.exoplatform.services.jcr.ext.replication.async;
 
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Vector;
+
 import org.exoplatform.services.jcr.core.WorkspaceContainerFacade;
-import org.exoplatform.services.jcr.core.nodetype.NodeTypeDataManager;
-import org.exoplatform.services.jcr.dataflow.DataManager;
-import org.exoplatform.services.jcr.dataflow.ItemState;
-import org.exoplatform.services.jcr.datamodel.NodeData;
+import org.exoplatform.services.jcr.dataflow.ItemStateChangesLog;
+import org.exoplatform.services.jcr.dataflow.TransactionChangesLog;
+import org.exoplatform.services.jcr.dataflow.persistent.ItemsPersistenceListener;
 import org.exoplatform.services.jcr.ext.BaseStandaloneTest;
-import org.exoplatform.services.jcr.ext.replication.async.storage.ChangesStorage;
+import org.exoplatform.services.jcr.ext.replication.async.storage.ChangesFile;
 import org.exoplatform.services.jcr.ext.replication.async.transport.AsyncChannelManager;
 import org.exoplatform.services.jcr.ext.replication.async.transport.Member;
-import org.exoplatform.services.jcr.impl.core.NodeImpl;
 import org.exoplatform.services.jcr.impl.dataflow.persistent.CacheableWorkspaceDataManager;
-import org.jgroups.stack.IpAddress;
+import org.jgroups.Address;
 
 /**
  * Created by The eXo Platform SAS.
@@ -37,79 +41,131 @@ import org.jgroups.stack.IpAddress;
  * @author <a href="mailto:alex.reshetnyak@exoplatform.com.ua">Alex Reshetnyak</a>
  * @version $Id: TestTransmitterChanges.java 111 2008-11-11 11:11:11Z rainf0x $
  */
-public class TestTransmitterChanges extends BaseStandaloneTest {
-  private static final String CH_CONFIG = "TCP(" + "start_port=7700;"
-                                            + "oob_thread_pool.queue_max_size=100;"
-                                            + "thread_naming_pattern=cl;"
-                                            + "use_concurrent_stack=true;"
-                                            + "oob_thread_pool.rejection_policy=Run;"
-                                            + "discard_incompatible_packets=true;"
-                                            + "thread_pool.max_threads=40;"
-                                            + "oob_thread_pool.enabled=false;"
-                                            + "oob_thread_pool.max_threads=20;" + "loopback=false;"
-                                            + "oob_thread_pool.keep_alive_time=5000;"
-                                            + "thread_pool.queue_enabled=false;"
-                                            + "oob_thread_pool.queue_enabled=false;"
-                                            + "max_bundle_size=64000;"
-                                            + "thread_pool.queue_max_size=100;"
-                                            + "thread_pool.enabled=false;"
-                                            + "enable_diagnostics=true;" + "max_bundle_timeout=30;"
-                                            + "oob_thread_pool.min_threads=8;"
-                                            + "use_incoming_packet_handler=true;"
-                                            + "thread_pool.rejection_policy=Run;"
-                                            + "bind_addr=$bind-ip-address;"
-                                            + "thread_pool.min_threads=8;"
-                                            + "thread_pool.keep_alive_time=5000;"
-                                            + "enable_bundling=true)" + ":MPING(" + "timeout=2000;"
-                                            + "num_initial_members=8;" + "mcast_port=34526;"
-                                            + "mcast_addr=224.0.0.1)" + ":FD(" + "timeout=2000;"
-                                            + "max_tries=5;" + "shun=true)" + ":FD_SOCK"
-                                            + ":VERIFY_SUSPECT(timeout=1500)" + ":pbcast.NAKACK("
-                                            + "max_xmit_size=60000;"
-                                            + "print_stability_history_on_failed_xmit=true;"
-                                            + "use_mcast_xmit=false;"
-                                            + "gc_lag=0;discard_delivered_msgs=true;"
-                                            + "retransmit_timeout=300,600,1200,2400,4800)"
-                                            + ":pbcast.STABLE(" + "stability_delay=1000;"
-                                            + "desired_avg_gossip=50000;" + "max_bytes=8000000)"
-                                            + ":pbcast.GMS(" + "print_local_addr=true;"
-                                            + "join_timeout=3000;" + "view_bundling=true;"
-                                            + "join_retry_timeout=2000;" + "shun=true;"
-                                            + "merge_leader=true;"
-                                            + "reject_join_from_existing_member=true)";
+public class TestTransmitterChanges extends BaseStandaloneTest implements ItemsPersistenceListener {
+  private static final String         CH_CONFIG = "TCP("
+                                                    + "start_port=7700;"
+                                                    + "oob_thread_pool.queue_max_size=100;"
+                                                    + "thread_naming_pattern=cl;"
+                                                    + "use_concurrent_stack=true;"
+                                                    + "oob_thread_pool.rejection_policy=Run;"
+                                                    + "discard_incompatible_packets=true;"
+                                                    + "thread_pool.max_threads=40;"
+                                                    + "oob_thread_pool.enabled=false;"
+                                                    + "oob_thread_pool.max_threads=20;"
+                                                    + "loopback=false;"
+                                                    + "oob_thread_pool.keep_alive_time=5000;"
+                                                    + "thread_pool.queue_enabled=false;"
+                                                    + "oob_thread_pool.queue_enabled=false;"
+                                                    + "max_bundle_size=64000;"
+                                                    + "thread_pool.queue_max_size=100;"
+                                                    + "thread_pool.enabled=false;"
+                                                    + "enable_diagnostics=true;"
+                                                    + "max_bundle_timeout=30;"
+                                                    + "oob_thread_pool.min_threads=8;"
+                                                    + "use_incoming_packet_handler=true;"
+                                                    + "thread_pool.rejection_policy=Run;"
+                                                    + "bind_addr=$bind-ip-address;"
+                                                    + "thread_pool.min_threads=8;"
+                                                    + "thread_pool.keep_alive_time=5000;"
+                                                    + "enable_bundling=true)"
+                                                    + ":MPING("
+                                                    + "timeout=2000;"
+                                                    + "num_initial_members=8;"
+                                                    + "mcast_port=34526;"
+                                                    + "mcast_addr=224.0.0.1)"
+                                                    + ":FD("
+                                                    + "timeout=2000;"
+                                                    + "max_tries=5;"
+                                                    + "shun=true)"
+                                                    + ":FD_SOCK"
+                                                    + ":VERIFY_SUSPECT(timeout=1500)"
+                                                    + ":pbcast.NAKACK("
+                                                    + "max_xmit_size=60000;"
+                                                    + "print_stability_history_on_failed_xmit=true;"
+                                                    + "use_mcast_xmit=false;"
+                                                    + "gc_lag=0;discard_delivered_msgs=true;"
+                                                    + "retransmit_timeout=300,600,1200,2400,4800)"
+                                                    + ":pbcast.STABLE(" + "stability_delay=1000;"
+                                                    + "desired_avg_gossip=50000;"
+                                                    + "max_bytes=8000000)" + ":pbcast.GMS("
+                                                    + "print_local_addr=true;"
+                                                    + "join_timeout=3000;" + "view_bundling=true;"
+                                                    + "join_retry_timeout=2000;" + "shun=true;"
+                                                    + "merge_leader=true;"
+                                                    + "reject_join_from_existing_member=true)";
 
-  private static final String CH_NAME   = "AsyncRepCh";
+  private static final String         IP_ADRESS_TEMPLATE = "[$]bind-ip-address";
+  
+  private static final String         CH_NAME   = "AsyncRepCh";
 
-  private static final int    priority  = 100;
+  private static final int            priority  = 50;
+  
+  private static final String         bindAddress = "192.168.0.15";  
+
+  private List<TransactionChangesLog> tclList   = new ArrayList<TransactionChangesLog>();
 
   public void tearDown() throws Exception {
     super.tearDown();
   }
 
+  /**
+   * testGetExport.
+   *
+   * @throws Exception
+   */
   public void testGetExport() throws Exception {
 
     WorkspaceContainerFacade wsc = repository.getWorkspaceContainer(session.getWorkspace()
                                                                            .getName());
-    
-    
+    CacheableWorkspaceDataManager dm = (CacheableWorkspaceDataManager) wsc.getComponent(CacheableWorkspaceDataManager.class);
+
+    dm.addItemPersistenceListener(this);
 
     // create node
 
-    NodeImpl n = (NodeImpl) root.addNode("testNode", "nt:unstructured");
+    for (int i = 0; i < 10; i++)
+      root.addNode("testNode_" + i, "nt:unstructured");
 
-    NodeData d = (NodeData) n.getData();
     root.save();
 
+    List<ChangesFile> cfList = new ArrayList<ChangesFile>();
 
-    AsyncChannelManager channel = new AsyncChannelManager(CH_CONFIG, CH_NAME);
+    for (TransactionChangesLog tcl : tclList) {
+      ChangesFile cf = new ChangesFile("ajgdjagsdjksasdasd", Calendar.getInstance()
+                                                                     .getTimeInMillis());
+
+      ObjectOutputStream oos = new ObjectOutputStream(cf.getOutputStream());
+
+      oos.writeObject(tcl);
+      oos.flush();
+
+      cfList.add(cf);
+    }
+    
+    String chConfig = CH_CONFIG.replaceAll(IP_ADRESS_TEMPLATE, bindAddress);
+    
+    AsyncChannelManager channel = new AsyncChannelManager(chConfig, CH_NAME);
 
     AsyncTransmitter transmitter = new AsyncTransmitterImpl(channel, priority);
 
-    DataManager dm = (DataManager) wsc.getComponent(CacheableWorkspaceDataManager.class);
     channel.connect();
-
     
+    
+    //subscribers
+    Vector<Address> addresses = channel.getDispatcher().getChannel().getView().getMembers();
+    addresses.remove(channel.getDispatcher().getChannel().getLocalAddress());
+    
+    
+    List<Member> memberList = new ArrayList<Member>();
+    for (Address addr : addresses)
+      memberList.add(new Member(addr));
+    
+    
+    transmitter.sendChanges(cfList,memberList);
+  }
 
+  public void onSaveItems(ItemStateChangesLog itemStates) {
+    tclList.add((TransactionChangesLog) itemStates);
   }
 
 }
