b4cfd441f6b193985f0563497af08326dcc46a16
Sergey Karpenko <sergey.karpenko@exoplatform.com>
2009-01-09 14:53:04 +0000

    LocalStorage implemented

--- component/ext/src/main/java/org/exoplatform/services/jcr/ext/replication/async/storage/LocalStorageImpl.java
+++ component/ext/src/main/java/org/exoplatform/services/jcr/ext/replication/async/storage/LocalStorageImpl.java
@@ -16,32 +16,130 @@
  */
 package org.exoplatform.services.jcr.ext.replication.async.storage;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.exoplatform.services.jcr.dataflow.ChangesLogIterator;
 import org.exoplatform.services.jcr.dataflow.ItemState;
 import org.exoplatform.services.jcr.dataflow.ItemStateChangesLog;
+import org.exoplatform.services.jcr.dataflow.PlainChangesLog;
+import org.exoplatform.services.jcr.dataflow.PlainChangesLogImpl;
+import org.exoplatform.services.jcr.dataflow.TransactionChangesLog;
+import org.exoplatform.services.jcr.datamodel.ValueData;
+import org.exoplatform.services.jcr.ext.replication.async.transport.Member;
+import org.exoplatform.services.jcr.impl.dataflow.TransientPropertyData;
+import org.exoplatform.services.jcr.impl.dataflow.TransientValueData;
+import org.exoplatform.services.log.ExoLogger;
 
 /**
- * Created by The eXo Platform SAS.
+ * Created by The eXo Platform SAS. <br/>Date: 26.12.2008
  * 
- * <br/>Date: 26.12.2008
- *
- * @author <a href="mailto:peter.nedonosko@exoplatform.com.ua">Peter Nedonosko</a> 
+ * @author <a href="mailto:peter.nedonosko@exoplatform.com.ua">Peter Nedonosko</a>
  * @version $Id$
  */
 public class LocalStorageImpl implements LocalStorage {
 
-  protected final String storagePath;
+  protected static final Log LOG = ExoLogger.getLogger("jcr.LocalStorageImpl");
+
+  protected final String     storagePath;
 
   public LocalStorageImpl(String storagePath) {
     this.storagePath = storagePath;
   }
-  
+
   public ChangesStorage<ItemState> getLocalChanges() {
-    // TODO Auto-generated method stub
-    return null;
+    File incomStorage = new File(storagePath);
+
+    String[] fileNames = incomStorage.list();
+    // Sort names in ascending mode
+    java.util.Arrays.sort(fileNames);
+
+    List<ChangesFile> chFiles = new ArrayList<ChangesFile>();
+    for (int j = 0; j < fileNames.length; j++) {
+      File ch = new File(incomStorage, fileNames[j]);
+      chFiles.add(new ChangesFile(ch, "", Long.parseLong(fileNames[j])));
+    }
+    // TODO make correct Member object creation
+    ChangesLogStorage<ItemState> changeStorage = new ChangesLogStorage<ItemState>(chFiles,
+                                                                                  new Member(null,
+                                                                                             0));
+    return changeStorage;
   }
 
   public void onSaveItems(ItemStateChangesLog itemStates) {
-    // TODO Auto-generated method stub
+    try {
+      ChangesFile file = createChangesFile();
+      ObjectOutputStream out = new ObjectOutputStream(file.getOutputStream());
+
+      TransactionChangesLog log = filterChangesLog((TransactionChangesLog) itemStates);
+
+      out.writeObject(log);
+      out.close();
+      file.finishWrite();
+    } catch (IOException e) {
+      LOG.error("" + e, e); // TODO
+    }
+  }
+
+  private ChangesFile createChangesFile() throws IOException {
+    return new ChangesFile("", System.currentTimeMillis(), storagePath);
+  }
+
+  private TransactionChangesLog filterChangesLog(TransactionChangesLog log) throws IOException {
+    // Change all TransientValueData to ReplicableValueData
+    ChangesLogIterator chIt = log.getLogIterator();
+
+    TransactionChangesLog result = new TransactionChangesLog();
+
+    while (chIt.hasNextLog()) {
+      PlainChangesLog plog = chIt.nextLog();
+
+      Iterator<ItemState> srcIt = plog.getAllStates().iterator();
+      List<ItemState> destlist = new ArrayList<ItemState>();
+      while (srcIt.hasNext()) {
+
+        ItemState item = srcIt.next();
+
+        if (item.isNode() == true) {
+          // skip nodes
+          destlist.add(item);
+        } else {
+          TransientPropertyData prop = (TransientPropertyData) item.getData();
+          Iterator<ValueData> valIt = prop.getValues().iterator();
+          List<ValueData> destVals = new ArrayList<ValueData>();
+
+          while (valIt.hasNext()) {
+            TransientValueData val = (TransientValueData) valIt.next();
+            ReplicableValueData dest;
+            if (val.isByteArray()) {
+              dest = new ReplicableValueData(val.getAsByteArray(), val.getOrderNumber());
+            } else {
+              dest = new ReplicableValueData(val.getSpoolFile(), val.getOrderNumber());
+            }
+            destVals.add(dest);
+          }
+          // rewrite values
+          prop.setValues(destVals);
+
+          // create new ItemState
+          ItemState nItem = new ItemState(prop,
+                                          item.getState(),
+                                          false,
+                                          item.getAncestorToSave(),
+                                          true);
+          destlist.add(nItem);
+        }
+      }
+      // create new palin changeslog
+      result.addLog(new PlainChangesLogImpl(destlist, plog.getSessionId(), plog.getEventType()));
+    }
+
+    return result;
 
   }
 
