bdbce0b6a8155da87964e650bf4bdb191a105c03
Peter Nedonosko <pnedonosko@exoplatform.com>
2009-01-12 11:00:10 +0000

    Merge with trunk r.24740 (SQL i18n tests fix)

--- component/core/src/test/java/org/exoplatform/services/jcr/usecases/query/TestI18NSQLQuery.java
+++ component/core/src/test/java/org/exoplatform/services/jcr/usecases/query/TestI18NSQLQuery.java
@@ -16,12 +16,9 @@
  */
 package org.exoplatform.services.jcr.usecases.query;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
-import javax.jcr.Session;
+import javax.jcr.RepositoryException;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
@@ -29,48 +26,32 @@ import javax.jcr.query.QueryResult;
 import org.exoplatform.services.jcr.usecases.BaseUsecasesTest;
 
 /**
- * Created by The eXo Platform SAS Author : Anh Nguyen ntuananh.vn@gmail.com Nov 15, 2007
+ * Created by The eXo Platform SAS Author : Anh Nguyen ntuananh.vn@gmail.com Nov
+ * 15, 2007
  */
 
 public class TestI18NSQLQuery extends BaseUsecasesTest {
 
   private static String[] input;
 
+  static {
+    input = new String[] { "ngocanh's o'clock" };
+  }
+
   public void testI18NQueryPath() throws Exception {
 
-    // Create nodes
-    Session session = repository.getSystemSession(repository.getSystemWorkspaceName());
+    // Create node
+    try {
+      String content = input[0];
+      root.addNode(content, "nt:unstructured");
+      root.save();
+    } catch (RepositoryException e) {
+      // OK there must be exception - path is illegal
 
-    input = readInputText("testi18n.txt").split("\n");
-
-    // Removing char '\r' (for DOS/Windows OS)
-    for (int k = 0; k < input.length; k++) {
-      if (input[k].endsWith("\r")) {
-        input[k] = input[k].substring(0, input[k].length() - 1);
-      }
     }
 
-    Node rootNode = session.getRootNode();
-
-    for (int i = 0; i < input.length; i++) {
-      String content = input[i];
-      rootNode.addNode(content, "nt:unstructured");
-    }
-    rootNode.save();
-
-    // Do Query by jcr:path
-    for (int i = 0; i < input.length; i++) {
-      String searchInput = input[i];
-
-      String sqlQuery = "select * from nt:unstructured where jcr:path like '/" + searchInput + "' ";
-
-      QueryManager manager = session.getWorkspace().getQueryManager();
-      Query query = manager.createQuery(sqlQuery, Query.SQL);
-      QueryResult queryResult = query.execute();
-      NodeIterator iter = queryResult.getNodes();
-
-      assertEquals(1, iter.getSize());
-    }
+    // String sqlQuery = "select * from nt:unstructured where jcr:path like
+    // '/ngocanh''s o''clock' ";
 
   }
 
@@ -80,56 +61,23 @@ public class TestI18NSQLQuery extends BaseUsecasesTest {
     // alway empty
 
     // Create nodes
-    Session session = repository.getSystemSession(repository.getSystemWorkspaceName());
+    String content = input[0];
 
-    Node rootNode = session.getRootNode();
-    for (int i = 0; i < input.length; i++) {
-      String content = input[i];
+    Node childNode = root.addNode("testNode", "nt:unstructured");
+    childNode.setProperty("exo:testi18n", content);
 
-      Node childNode = rootNode.addNode(String.valueOf(i), "nt:unstructured");
-      childNode.setProperty("exo:testi18n", content);
-
-    }
-    rootNode.save();
+    root.save();
 
     // Do Query by properties
-    for (int i = 0; i < input.length; i++) {
 
-      String searchInput = input[i];
+    String sqlQuery = "select * from nt:unstructured where exo:testi18n like 'ngocanh''s o''clock' ";
 
-      String sqlQuery = "select * from nt:unstructured where exo:testi18n like '" + searchInput
-          + "' ";
+    QueryManager manager = session.getWorkspace().getQueryManager();
+    Query query = manager.createQuery(sqlQuery, Query.SQL);
+    QueryResult queryResult = query.execute();
 
-      QueryManager manager = session.getWorkspace().getQueryManager();
-      Query query = manager.createQuery(sqlQuery, Query.SQL);
-      QueryResult queryResult = query.execute();
-
-      NodeIterator iter = queryResult.getNodes();
-      assertEquals(1, iter.getSize());
-    }
-
+    NodeIterator iter = queryResult.getNodes();
+    assertEquals(1, iter.getSize());
   }
 
-  private static String readInputText(String fileName) {
-
-    try {
-      InputStream is = TestI18NSQLQuery.class.getResourceAsStream(fileName);
-      ByteArrayOutputStream output = new ByteArrayOutputStream();
-
-      int r = is.available();
-      byte[] bs = new byte[r];
-      while (r > 0) {
-        r = is.read(bs);
-        if (r > 0) {
-          output.write(bs, 0, r);
-        }
-        r = is.available();
-      }
-      is.close();
-      return output.toString();
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
 }
