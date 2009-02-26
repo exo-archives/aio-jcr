bc6031e7b6f3c6842839f3f2cd924a5e928a1401
Sergey Karpenko <sergey.karpenko@exoplatform.com>
2009-02-26 09:43:31 +0000

    JCR-794: test.sh  fixed

--- applications/java/standalone/backupconsole/bin/test.sh
+++ applications/java/standalone/backupconsole/bin/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./jcrbackup.sh "eXo RESR services" root:exo 127.0.0.1:8080 status /repository/production
+./jcrbackup.sh "eXo REST services" root:exo 127.0.0.1:8080 status /repository/production
