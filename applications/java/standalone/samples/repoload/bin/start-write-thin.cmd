e2a4a70e3febdc3ba851bb7cc0c1483906565e1e
Peter Nedonosko <pnedonosko@exoplatform.com>
2009-03-26 15:01:53 +0000

    version change for scripts and build configs

--- applications/java/standalone/samples/repoload/bin/start-write-thin.cmd
+++ applications/java/standalone/samples/repoload/bin/start-write-thin.cmd
@@ -1,1 +1,1 @@
-java -Duser.language=en -Duser.region=us -jar exo.jcr.applications.repoload-1.10.3-SNAPSHOT.jar -conf=".\bin\config\configuration-thin.xml" -root="/testStorage/root1" -tree="10-100-100-100" -vdfile=".\bin\img.tif" -repo="repository" -ws="production" -readtree="false" 
+java -Duser.language=en -Duser.region=us -jar exo.jcr.applications.repoload-1.10.4-SNAPSHOT.jar -conf=".\bin\config\configuration-thin.xml" -root="/testStorage/root1" -tree="10-100-100-100" -vdfile=".\bin\img.tif" -repo="repository" -ws="production" -readtree="false" 
--- applications/java/standalone/samples/repoload/bin/start_ora.bat
+++ applications/java/standalone/samples/repoload/bin/start_ora.bat
@@ -1,3 +1,3 @@
-java -Duser.language=en -Duser.region=us -jar repoload-1.10.3-SNAPSHOT.jar -conf="test-configuration.ora-thin.xml" -root="testroot_11__s2" -tree="10-5-2-10" -vdfile="img.tif" -repo="db1" -ws="ws" -readtree="true"
+java -Duser.language=en -Duser.region=us -jar repoload-1.10.4-SNAPSHOT.jar -conf="test-configuration.ora-thin.xml" -root="testroot_11__s2" -tree="10-5-2-10" -vdfile="img.tif" -repo="db1" -ws="ws" -readtree="true"
 
 pause
