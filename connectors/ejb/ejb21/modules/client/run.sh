903911fa5d9d2eda186a641ea9a0a32dcc3d1b4b
Peter Nedonosko <pnedonosko@exoplatform.com>
2009-04-15 14:00:04 +0000

    version 1.11.1 in command files abd build descriptors

--- connectors/ejb/ejb21/modules/client/run.sh
+++ connectors/ejb/ejb21/modules/client/run.sh
@@ -7,11 +7,11 @@ fi
 
 $JAVA_HOME/bin/java -Djava.security.manager=default  \
 -Djava.security.policy=$JONAS_ROOT/conf/java.policy \
--cp $JONAS_ROOT/lib/apps/exo.core.component.security.core-2.2.1-SNAPSHOT.jar:\
-$JONAS_ROOT/lib/apps/exo.ws.commons-2.0.1-SNAPSHOT.jar:\
-$JONAS_ROOT/lib/apps/exo.ws.rest.core-2.0.1-SNAPSHOT.jar:\
-$JONAS_ROOT/lib/apps/exo.ws.rest.ext-2.0.1-SNAPSHOT.jar:\
+-cp $JONAS_ROOT/lib/apps/exo.core.component.security.core-2.2.1.jar:\
+$JONAS_ROOT/lib/apps/exo.ws.commons-2.0.1.jar:\
+$JONAS_ROOT/lib/apps/exo.ws.rest.core-2.0.1.jar:\
+$JONAS_ROOT/lib/apps/exo.ws.rest.ext-2.0.1.jar:\
 $JONAS_ROOT/lib/apps/jsr311-api-1.0.jar:\
 $JONAS_ROOT/lib/client.jar:\
-exo.jcr.connectors.ejb21.bean-1.11.1-SNAPSHOT.jar \
-org.objectweb.jonas.client.ClientContainer exo.jcr.connectors.ejb21.client-1.11.1-SNAPSHOT.jar $1 $2
+exo.jcr.connectors.ejb21.bean-1.11.1.jar \
+org.objectweb.jonas.client.ClientContainer exo.jcr.connectors.ejb21.client-1.11.1.jar $1 $2
