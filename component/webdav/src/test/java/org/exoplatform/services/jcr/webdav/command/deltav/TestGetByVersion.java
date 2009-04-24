9bdea13bdcd7f87d19392ec00f365df39acee8b6
Vitaliy Parfonov <vparfonov@exoplatform.com>
2009-04-24 14:46:42 +0000

    JCR-890

---
+++ component/webdav/src/test/java/org/exoplatform/services/jcr/webdav/command/deltav/TestGetByVersion.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+
+package org.exoplatform.services.jcr.webdav.command.deltav;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.version.Version;
+import javax.jcr.version.VersionException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.exoplatform.common.http.HTTPStatus;
+import org.exoplatform.services.jcr.webdav.BaseStandaloneTest;
+import org.exoplatform.services.jcr.webdav.Range;
+import org.exoplatform.services.jcr.webdav.WebDavConstants.WebDAVMethods;
+import org.exoplatform.services.jcr.webdav.command.GetCommand;
+import org.exoplatform.services.jcr.webdav.command.PutCommand;
+import org.exoplatform.services.jcr.webdav.lock.NullResourceLocksHolder;
+import org.exoplatform.services.jcr.webdav.util.TextUtil;
+import org.exoplatform.services.jcr.webdav.utils.TestUtils;
+import org.exoplatform.services.rest.impl.ContainerResponse;
+
+/**
+ * Created by The eXo Platform SAS.
+ * Author : Vitaly Guly <gavrikvetal@gmail.com>
+ * @version $Id: $
+ */
+
+public class TestGetByVersion extends BaseStandaloneTest {
+  
+  private Node getByVersionNode;
+
+  
+    
+  private void assertResponseContent(String path, String versionName, String content) throws IOException, VersionException, UnsupportedRepositoryOperationException, PathNotFoundException, RepositoryException {
+    Version version = session.getRootNode().getNode(TextUtil.relativizePath(path)).getVersionHistory().getVersion(versionName);
+    String receivedContent = version.getNode("jcr:frozenNode").getNode("jcr:content").getProperty("jcr:data").getString();
+    assertEquals(content, receivedContent);    
+  }
+  
+  public void testGetByVersion1() throws Exception {
+    String CONTENT1 = "TEST BASE FILE CONTENT..";
+    String CONTENT2 = "TEST CONTENT FOR SECOND VERSION..";
+    String CONTENT3 = "TEST CONTENT FOR THIRD VERSION OF THE FILE..";
+    
+//    NullResourceLocksHolder lockHolder = new NullResourceLocksHolder();
+    
+    String path = TestUtils.getFileName();
+    Node node = TestUtils.addContent(session, path, new ByteArrayInputStream(CONTENT1.getBytes()), defaultFileNodeType, MediaType.TEXT_PLAIN);
+    createVersion(node, new ByteArrayInputStream(CONTENT2.getBytes()), MediaType.TEXT_PLAIN, new ArrayList<String>());
+    createVersion(node, new ByteArrayInputStream(CONTENT3.getBytes()), MediaType.TEXT_PLAIN, new ArrayList<String>());
+    assertResponseContent(path, "1", CONTENT1);
+    assertResponseContent(path, "2", CONTENT2);
+    assertResponseContent(path, "3", CONTENT3);
+  }
+  
+  
+  private final void createVersion(Node fileNode,
+                                   InputStream inputStream,
+                                   String mimeType,
+                                   List<String> mixins) throws RepositoryException {
+    if (!fileNode.isNodeType("mix:versionable")) {
+      if (fileNode.canAddMixin("mix:versionable")) {
+        fileNode.addMixin("mix:versionable");
+        fileNode.getSession().save();
+      }
+      fileNode.checkin();
+      fileNode.getSession().save();
+    }
+
+    if (!fileNode.isCheckedOut()) {
+      fileNode.checkout();
+      fileNode.getSession().save();
+    }
+
+    updateContent(fileNode, inputStream, mimeType, mixins);
+    fileNode.getSession().save();
+    fileNode.checkin();
+    fileNode.getSession().save();
+  }
+
+  private final void updateContent(Node node,
+                                   InputStream inputStream,
+                                   String mimeType,
+                                   List<String> mixins) throws RepositoryException {
+    
+    Node content = node.getNode("jcr:content");
+    content.setProperty("jcr:mimeType", mimeType);
+    content.setProperty("jcr:lastModified", Calendar.getInstance());
+    content.setProperty("jcr:data", inputStream);
+    
+    while(mixins.iterator().hasNext()){
+      content.addMixin(mixins.iterator().next());
+    }
+  }
+
+  @Override
+  protected String getRepositoryName() {
+    return null;
+  }
+
+}
---
+++ component/webdav/src/test/java/org/exoplatform/services/jcr/webdav/command/deltav/TestUnCkeckOut.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+
+package org.exoplatform.services.jcr.webdav.command.deltav;
+
+import java.io.ByteArrayInputStream;
+
+import javax.jcr.Node;
+import javax.ws.rs.core.MediaType;
+
+import org.exoplatform.common.http.HTTPStatus;
+import org.exoplatform.services.jcr.webdav.BaseStandaloneTest;
+import org.exoplatform.services.jcr.webdav.utils.TestUtils;
+import org.exoplatform.services.rest.impl.ContainerResponse;
+
+/**
+ * Created by The eXo Platform SAS.
+ * Author : Vitaly Guly <gavrikvetal@gmail.com>
+ * @version $Id: $
+ */
+
+public class TestUnCkeckOut extends BaseStandaloneTest {
+
+  
+  public void testUnCkeckOut() throws Exception {
+    String path = TestUtils.getFileName();
+    Node  node = TestUtils.addContent(session, path, new ByteArrayInputStream(TestUtils.getFileContent().getBytes()), defaultFileNodeType, MediaType.TEXT_PLAIN);
+    ContainerResponse response = service("UNCHECKOUT", getPathWS() + path, "", null, null); 
+    assertEquals(HTTPStatus.CONFLICT, response.getStatus());
+    if (!node.isNodeType("mix:versionable")) {
+      node.addMixin("mix:versionable");
+      session.save();
+    }
+    response = service("UNCHECKOUT", getPathWS() + path, "", null, null);
+    assertEquals(HTTPStatus.INTERNAL_ERROR, response.getStatus());
+    node.checkin();
+    node.checkout();
+    response = service("UNCHECKOUT", getPathWS() + path, "", null, null);
+    assertEquals(HTTPStatus.OK, response.getStatus());
+  }
+
+
+
+  @Override
+  protected String getRepositoryName() {
+    return null;
+  }
+  
+
+}
+
