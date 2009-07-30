c4f8bc331801a011fc9eab1669b28f7ba29418d1
Peter Nedonosko <pnedonosko@exoplatform.com>
2009-07-30 08:31:00 +0000

    JCR-1116: jra plugin 2.2; JCR core tests run script

--- component/core/test-dev.cmd
+++ component/core/test-dev.cmd
@@ -1,3 +1,3 @@
-@set MAVEN_OPTS=-Duser.language=en -Duser.region=us %MAVEN_OPTS% -Dexo.test.skip=true -Dexo.tck.skip=true -Dexo.devtest.skip=false -Dexo.test.forkMode=never 
+@set MAVEN_OPTS=-Duser.language=en -Duser.region=us %MAVEN_OPTS% -Dexo.test.skip=true -Dexo.tck.skip=true -Dexo.devtest.skip=false -Dexo.test.forkMode=once 
 
 @start mvn clean test
