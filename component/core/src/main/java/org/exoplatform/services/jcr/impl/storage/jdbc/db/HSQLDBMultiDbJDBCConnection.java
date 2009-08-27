53238f4c79cfc5dfed16a3e8f216aea27e8c14c4
Denis Grebenyuk <dezder@bk.ru>
2009-08-27 13:01:30 +0000

    Add license to classes

--- component/core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/db/HSQLDBMultiDbJDBCConnection.java
+++ component/core/src/main/java/org/exoplatform/services/jcr/impl/storage/jdbc/db/HSQLDBMultiDbJDBCConnection.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
 package org.exoplatform.services.jcr.impl.storage.jdbc.db;
 
 import java.io.File;
@@ -7,34 +23,38 @@ import java.sql.SQLException;
 import org.exoplatform.services.jcr.impl.util.io.FileCleaner;
 import org.exoplatform.services.jcr.storage.value.ValueStoragePluginProvider;
 
+/**
+ * Created by The eXo Platform SAS
+ * 
+ * 20.08.2009
+ * 
+ * @author <a href="mailto:dezder@bk.ru">Denis Grebenyuk</a>
+ * @version $Id: HSQLDBMultiDbJDBCConnection.java 36070 2009-08-26 11:01:14Z
+ *          pnedonosko $
+ */
 public class HSQLDBMultiDbJDBCConnection extends MultiDbJDBCConnection {
 
-  public HSQLDBMultiDbJDBCConnection(Connection dbConnection,
-                                     boolean readOnly,
-                                     String containerName,
-                                     ValueStoragePluginProvider valueStorageProvider,
-                                     int maxBufferSize,
-                                     File swapDirectory,
-                                     FileCleaner swapCleaner) throws SQLException {
-    super(dbConnection,
-          readOnly,
-          containerName,
-          valueStorageProvider,
-          maxBufferSize,
-          swapDirectory,
-          swapCleaner);
-  }
+	public HSQLDBMultiDbJDBCConnection(Connection dbConnection,
+			boolean readOnly, String containerName,
+			ValueStoragePluginProvider valueStorageProvider, int maxBufferSize,
+			File swapDirectory, FileCleaner swapCleaner) throws SQLException {
+		super(dbConnection, readOnly, containerName, valueStorageProvider,
+				maxBufferSize, swapDirectory, swapCleaner);
+	}
 
-  @Override
-  protected void prepareQueries() throws SQLException {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void prepareQueries() throws SQLException {
 
-    super.prepareQueries();
-    FIND_PROPERTY_BY_NAME = "select V.DATA"
-        + " from JCR_MITEM I, JCR_MVALUE V"
-        + " where I.PARENT_ID=? and I.I_CLASS=2 and I.NAME=? and I.ID=V.PROPERTY_ID order by V.ORDER_NUM";
-    FIND_NODES_BY_PARENTID = "select * from JCR_MITEM" + " where PARENT_ID=? and I_CLASS=1"
-        + " order by N_ORDER_NUM";
-    FIND_PROPERTIES_BY_PARENTID = "select * from JCR_MITEM" + " where PARENT_ID=? and I_CLASS=2"
-        + " order by ID";
-  }
+		super.prepareQueries();
+		FIND_PROPERTY_BY_NAME = "select V.DATA"
+				+ " from JCR_MITEM I, JCR_MVALUE V"
+				+ " where I.PARENT_ID=? and I.I_CLASS=2 and I.NAME=? and I.ID=V.PROPERTY_ID order by V.ORDER_NUM";
+		FIND_NODES_BY_PARENTID = "select * from JCR_MITEM"
+				+ " where PARENT_ID=? and I_CLASS=1" + " order by N_ORDER_NUM";
+		FIND_PROPERTIES_BY_PARENTID = "select * from JCR_MITEM"
+				+ " where PARENT_ID=? and I_CLASS=2" + " order by ID";
+	}
 }
